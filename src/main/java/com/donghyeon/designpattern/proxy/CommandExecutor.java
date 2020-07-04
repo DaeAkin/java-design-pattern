e0878e14e7a1432e911fcc7ded397947c14992c0
민동현 <mindonghyeon890@gmail.com>
2020-07-04 10:40:43 +0900

    프록시 완성

--- src/main/java/com/donghyeon/designpattern/proxy/CommandExecutor.java
+++ src/main/java/com/donghyeon/designpattern/proxy/CommandExecutor.java
@@ -1,5 +1,5 @@
 package com.donghyeon.designpattern.proxy;
 
 public interface CommandExecutor {
-    public void runCommand(String cmd) throws Exception;
+     void runCommand(String cmd) throws Exception;
 }
--- src/main/java/com/donghyeon/designpattern/proxy/CommandExecutorImpl.java
+++ src/main/java/com/donghyeon/designpattern/proxy/CommandExecutorImpl.java
@@ -6,7 +6,6 @@ public class CommandExecutorImpl implements CommandExecutor {
 
     @Override
     public void runCommand(String cmd) throws IOException {
-        //많은 구현 소스들이 있다고 상상해주세요..ㅎ
         Runtime.getRuntime().exec(cmd);
         System.out.println("'" + cmd + "' 명령어가 실행됨.");
     }
